b72e17ffa0c8be27f9c3fb79bfc48ea7f5a46c15
ganmingkun <ganmingkun@gmail.com>
2018-08-22 16:24:28 +0800

    添加一个tushare抓取新闻脚本

---
+++ Lib/c.py
@@ -0,0 +1,12 @@
+#!user/bin/env python3  
+# -*- coding: gbk -*- 
+import tushare as ts
+import json
+
+#ts.get_latest_news() #默认获取最近80条新闻数据，只提供新闻类型、链接和标题
+print(ts.get_latest_news(top=1,show_content=True)) #显示最新5条新闻，并打印出新闻内容
+df =ts.get_latest_news(top=80,show_content=True)
+df.to_csv('D:/Result.csv')
+
+#f1 = open('a.txt','w')
+#f1.write(json.dumps( df.to_json(orient='table')))
